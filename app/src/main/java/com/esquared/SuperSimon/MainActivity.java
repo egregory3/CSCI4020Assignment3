301260949d637943de2218b0ff3fc5511d17c401
Eric Raymond <eraymond@my.apsu.edu>
2020-04-17 18:09:03 -0500

    Eric Branch Start

--- app/src/main/java/com/esquared/SuperSimon/MainActivity.java
+++ app/src/main/java/com/esquared/SuperSimon/MainActivity.java
@@ -3,6 +3,11 @@ package com.esquared.SuperSimon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LightingColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.StateListDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -10,115 +15,54 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    Button blue;
-    Button green;
-    Button red;
-    Button yellow;
-    boolean main = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button blue = findViewById(R.id.btn_blue);
+        addClickEffect(blue);
 
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), simon_classic.class);
+                startActivity(intent);
+            }
+        });
+
+        Button red = findViewById(R.id.btn_red);
+        addClickEffect(red);
+        Button green = findViewById(R.id.btn_green);
+        addClickEffect(green);
+        Button yellow = findViewById(R.id.btn_yellow);
+        addClickEffect(yellow);
     }
+
+
+
 
     @Override
     protected void onResume() {
-        main = true;
         super.onResume();
 
-        blue = findViewById(R.id.btn_blue);
-        red = findViewById(R.id.btn_red);
-        green = findViewById(R.id.btn_green);
-        yellow = findViewById(R.id.btn_yellow);
-        blue.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                processTouch(blue, event);
-                Intent intent = new Intent(MainActivity.this, simon_classic.class);
-                MainActivity.this.startActivity(intent);
+    }
 
-                return false;
-            }
-        });
 
-        red.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                processTouch(red, event);
-                return false;
-            }
-        });
+    void addClickEffect(View v)
+    {
+        Drawable dN = v.getBackground();
+        Drawable dP = v.getBackground().getConstantState().newDrawable();
+        dP.mutate();
+        dP.setColorFilter(new LightingColorFilter(0x77777777, 0x77777777));
 
-        green.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                processTouch(green, event);
-                return false;
-            }
-        });
-
-        yellow.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                processTouch(yellow, event);
-                return false;
-            }
-        });
-
+        StateListDrawable lD = new StateListDrawable();
+        lD.addState(new int[] {android.R.attr.state_pressed}, dP);
+        lD.addState(new int[] {}, dN);
+        v.setBackground(lD);
     }
 
 
 
-    boolean processTouch(View V, MotionEvent event){
-        new TouchProcessor(V, event);
-        return false;
-    }
-
-    public class TouchProcessor {
-        private View v;
-        private MotionEvent event;
-
-        public TouchProcessor(View V, MotionEvent event) {
-            v = V;
-            this.event = event;
-
-            Button myV = findViewById(v.getId());
-            Button btn = findViewById(R.id.btn_blue);
-            int bgDown = R.drawable.button_blue_down;
-            int bgUp = R.drawable.button_blue;
-
-
-            if (myV == yellow){
-                btn = findViewById(R.id.btn_yellow);
-                bgDown = R.drawable.button_yellow_down;
-                bgUp = R.drawable.button_yellow;
-            }else if (myV == red){
-                btn = findViewById(R.id.btn_red);
-                bgDown = R.drawable.button_red_down;
-                bgUp = R.drawable.button_red;
-            }else if (myV == green){
-                btn = findViewById(R.id.btn_green);
-                bgDown = R.drawable.button_green_down;
-                bgUp = R.drawable.button_green;
-            }
-
-                switch(event.getAction()) {
-                    case MotionEvent.ACTION_DOWN:
-                        Log.i("Button", "Blue Button Pressed");
-                        btn.setBackgroundResource(bgDown);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        btn.setBackgroundResource(bgUp);
-                        break;
-
-
-
-                }
-
-    }
-}}
-
-
+}
--- app/src/main/java/com/esquared/SuperSimon/simon_classic.java
+++ app/src/main/java/com/esquared/SuperSimon/simon_classic.java
@@ -2,6 +2,7 @@ package com.esquared.SuperSimon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -13,133 +14,51 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class simon_classic extends MainActivity {
-    Random random = new Random();
-    int nextFlash = 0;
+    Random randomNumber;
     ArrayList<Integer> pattern = new ArrayList<Integer>();
     ArrayList<Integer> CurrentRun = new ArrayList<Integer>();
     Button blue;
     Button green;
     Button red;
     Button yellow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_simon_classic);
-        blue = findViewById(R.id.btn_blue);
-        green = findViewById(R.id.btn_green);
-        red = findViewById(R.id.btn_red);
-        yellow = findViewById(R.id.btn_yellow);
+
+        Button blue = findViewById(R.id.btn_blue);
+        addClickEffect(blue);
+
+        blue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        Button red = findViewById(R.id.btn_red);
+        addClickEffect(red);
+        Button green = findViewById(R.id.btn_green);
+        addClickEffect(green);
+        Button yellow = findViewById(R.id.btn_yellow);
+        addClickEffect(yellow);
 
         blue.setText("");
         green.setText("");
         red.setText("");
         yellow.setText("");
 
-        blue.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                new TouchProcessor(v, event);
-                return false;
-            }
-        });
-        green.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                new TouchProcessor(v, event);
-                return false;
-            }
-        });
-        blue.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                new TouchProcessor(v, event);
-                return false;
-            }
-        });
-        yellow.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                new TouchProcessor(v, event);
-                return false;
-            }
-        });
 
     }
 
-    public void onResume(){
-        main = false;
+    public void onResume() {
+
         super.onResume();
 
-        try {
-            advance();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
 
     }
 
-    public void advance() throws InterruptedException {
-        nextFlash = random.nextInt(4);
-        pattern.add(nextFlash);
-        for(int i=0; i<pattern.size(); i++)
-        {
-
-            switch(pattern.get(i)){
-                case(0):{
-                    flash(blue);
-                }
-                case(1):{
-                    flash(green);
-                }
-                case(2):{
-                    flash(red);
-                }
-                case(3):{
-                    flash(yellow);
-                }
-            }
-        }
-    }
-    public void flash(View V) throws InterruptedException {
-        Button button = findViewById(V.getId());
-        int bgrd = 0, bgru = 0;
-        switch(V.getId()){
-            case(R.id.btn_blue):{
-                bgrd = R.drawable.button_blue_down;
-                Thread.sleep(500);
-                bgru = R.drawable.button_blue;
-                break;
-
-            }
-            case(R.id.btn_green):{
-                bgrd = R.drawable.button_green_down;
-                Thread.sleep(500);
-                bgru = R.drawable.button_green;
-                break;
-            }
-            case(R.id.btn_red):{
-                bgrd = R.drawable.button_red_down;
-                Thread.sleep(500);
-                bgru = R.drawable.button_red;
-                break;
-            }
-            case(R.id.btn_yellow):{
-                bgrd = R.drawable.button_yellow_down;
-                Thread.sleep(500);
-                bgru = R.drawable.button_yellow;
-                break;
-            }
 
 
-
-        }
-        button.setBackgroundResource(bgrd);
-        Log.i("Button State", "Down");
-        Thread.sleep(500);
-        button.setBackgroundResource(bgru);
-        Log.i("Button State", "up");
-        return;
-    }
 }
-
-
